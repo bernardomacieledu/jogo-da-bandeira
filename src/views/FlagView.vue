dae23c44471b114a2b6a5b7841b6a793d4d2984b
bernardomacieledu <bernardo.maciel.edu@gmail.com>
2025-09-16 22:31:58 -0300

    jogo das bandeiras

---
+++ src/views/FlagView.vue
@@ -0,0 +1,21 @@
+<script setup>
+import { useFlagStore } from '@/stores/flag'
+import { useAppStore } from '@/stores/app';
+const appStore = useAppStore();
+const flagStore = useFlagStore();
+appStore.sortearBandeiras(flagStore.flags);
+
+</script>
+
+<template>
+  <h2>{{ appStore.bandeiraEscolhida.name }}</h2>
+  <ul>
+    <li v-for="bandeira in appStore.bandeirasSorteadas" :key="bandeira.id">
+      <img :src="'flags/'+bandeira.image" alt="Imagem da Bandeira">
+    </li>
+  </ul>
+</template>
+
+<style>
+
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,10 @@
+<script setup>
+import { RouterLink } from 'vue-router'
+
+</script>
+
+<template>
+  <main>
+    <router-link to="flag-app">Iniciar</router-link>
+  </main>
+</template>

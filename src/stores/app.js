3fbc4d8f8b41c57ff0428d18d614e8a892daf9a2
bernardomacieledu <bernardo.maciel.edu@gmail.com>
2025-09-30 22:14:46 -0300

    Update app.js

--- src/stores/app.js
+++ src/stores/app.js
@@ -1,21 +1,22 @@
 import { ref } from 'vue'
 import { defineStore } from 'pinia'
 
+
 export const useAppStore = defineStore('app', () => {
   const bandeirasSorteadas = ref([])
   const quantidadeBandeiras = 9
   const bandeiraEscolhida = ref(null)
+  const pontos = ref(0)
+  
 
   function sortearBandeiras(bandeiras) {
     while(bandeirasSorteadas.value.length < quantidadeBandeiras){
       const elementoSorteio = Math.floor(Math.random() * 50);
       const posicao = bandeirasSorteadas.value.findIndex((x) => x.id === bandeiras[elementoSorteio].id);
-      console.log(posicao);
+      
       if (posicao == -1) {
         bandeirasSorteadas.value.push({...bandeiras[elementoSorteio]});
       }
-      console.log(elementoSorteio);
-      console.log(bandeirasSorteadas.value);
     }
     const correta = Math.floor(Math.random() * quantidadeBandeiras);
     bandeiraEscolhida.value = bandeirasSorteadas.value[correta];
